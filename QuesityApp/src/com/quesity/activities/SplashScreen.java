855bca3aaaecddce7454ea87ec56fd9128c6da6a
lior <liorz1984@gmail.com>
2013-09-22 01:11:36 +0300

    Added a request for user's details from facebook

--- QuesityApp/src/com/quesity/activities/SplashScreen.java
+++ QuesityApp/src/com/quesity/activities/SplashScreen.java
@@ -1,20 +1,22 @@
 package com.quesity.activities;
 
+import java.util.Arrays;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 import com.quesity.R;
 import com.quesity.fragments.LoadingProgressFragment;
-import com.quesity.general.Config;
 import com.quesity.general.Constants;
 import com.quesity.models.Account;
 import com.quesity.models.ModelsFactory;
@@ -23,140 +25,100 @@ import com.quesity.network.JSONPostRequestTypeGetter;
 
 public class SplashScreen extends FragmentActivity {
 
-	private static final int MAIN_LAYOUT_FADE_DURATION = 3000;
-	private static final int FADE_WAIT_DURATION = 2000;
-	private static final int LOGIN_BOX_FADE_DURATION = 1000;
-	
+	private boolean isResumed = false;
+
+	private UiLifecycleHelper uiHelper;
+	private LoginButton _loginBtn;
+	private Session.StatusCallback _sessionCallback = 
+		    new Session.StatusCallback() {
+		    @Override
+		    public void call(Session session, 
+		            SessionState state, Exception exception) {
+		        onSessionStateChange(session, state, exception);
+		    }
+	};
 	private LoadingProgressFragment _progress;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		_progress = new LoadingProgressFragment();
-
+		
 		setContentView(R.layout.activity_splash_screen);
-		animateMain();
+		_loginBtn = (LoginButton) findViewById(R.id.login_button);
 		
-		if ( isAlreadyLoggedIn() ) {
-			SharedPreferences prefs = getSharedPreferences(Constants.PREFS_NAME,MODE_PRIVATE);
-			String username = prefs.getString(Constants.PREF_USERNAME, null);
-			String password = prefs.getString(Constants.PREF_PASSWORD, null);
-			loginAction(username, password);
-		}
-		setLoginAction();		
+		_loginBtn.setReadPermissions(Arrays.asList("user_birthday", "email", "user_location"));
+		uiHelper = new UiLifecycleHelper(this, _sessionCallback);
+	    uiHelper.onCreate(savedInstanceState);
 		
 	}
 	
-	private boolean isAlreadyLoggedIn(){
-		SharedPreferences prefs = getSharedPreferences(Constants.PREFS_NAME,MODE_PRIVATE);
-		String username = prefs.getString(Constants.PREF_USERNAME, null);
-		String password = prefs.getString(Constants.PREF_PASSWORD, null);
-		
-		if (username != null && password != null ){
-			return true;
-		}
-		return false;
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    isResumed = true;
+	    uiHelper.onResume();
 	}
-	private void loginAction(String username, String password) {
-		Account account = new Account();
-		account.setPassword(password);
-		account.setUsername(username);
-		
-		final String json = ModelsFactory.getInstance().getJSONFromAccount(account);
-		new LoginTask(json).execute(Config.SERVER_URL + "/login");
-	}
-	
-	private void saveCredentials(String username, String password) {
-		SharedPreferences prefs = getSharedPreferences(Constants.PREFS_NAME,MODE_PRIVATE);
-		prefs.edit().putString(Constants.PREF_USERNAME, username)
-		.putString(Constants.PREF_PASSWORD, password).commit();
-	}
-	
-	private void setLoginAction() {
-		final EditText txt_username = (EditText) findViewById(R.id.txt_username);
-		final EditText txt_password = (EditText) findViewById(R.id.txt_password);
-		final Button btn_login = (Button) findViewById(R.id.btn_login);
-		
-		btn_login.setOnClickListener(new Button.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				final String username = txt_username.getText().toString();
-				final String password = txt_password.getText().toString();
-				loginAction(username,password);
-				saveCredentials(username, password);
-			}
-		});
-		
-	}
-	
-	private void showLoginBox() {
-		final View loginBox = findViewById(R.id.login_box);
-		loginBox.setVisibility(View.VISIBLE);
-	}
-	
-	private void animateLoginBox() {
-		final View loginBox = findViewById(R.id.login_box);
 
-		final Animation animation = getAnimationWithDuration(LOGIN_BOX_FADE_DURATION);
-		animation.setStartOffset(FADE_WAIT_DURATION + MAIN_LAYOUT_FADE_DURATION);
-		animation.setAnimationListener(new AnimationListener() {
-			
-			@Override
-			public void onAnimationStart(Animation animation) {
-				loginBox.setVisibility(View.VISIBLE);
-			}
-			
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-				
-			}
-			
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				
-			}
-		});
-		loginBox.setAnimation(animation);
-		
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    isResumed = true;
+	    uiHelper.onPause();
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
+	}
+	
+	private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
+	    // Only make changes if the activity is visible
+	    if (isResumed) {
+	        if (state.isOpened()) {
+	            // If the session state is open:
+	            // Show the authenticated fragment
+//	        	Intent intent = new Intent(this, QuesityMain.class);
+//	        	startActivity(intent);
+	        	Request newMeRequest = Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						if ( response.getError() == null  ){
+							Log.d("MainActivity", user.getId());
+							Log.d("MainActivity", session.getAccessToken());
+						}
+					}
+					
+					
+	        	});
+	        	newMeRequest.executeAsync();
+	        } else if (state.isClosed()) {
+	            // If the session state is closed:
+	            // Show the login fragment
+	        	Intent intent = new Intent(this, SplashScreen.class);
+	        	startActivity(intent);
+	        }
+	    }
 	}
 	
 	private void loadMainScreen() {
     	Intent intent = new Intent(this, QuesityMain.class);
     	startActivity(intent);
 	}
-	private Animation getAnimationWithDuration(int duration) {
-		Animation animation = new AlphaAnimation(0, 1);
-		animation.setInterpolator(new DecelerateInterpolator());
-		animation.setDuration(duration);
-		return animation;
-	}
 
-	private void animateMain() {
-		final View main_layout = findViewById(R.id.splash_main_layout);
-		Animation fadeIn_main = getAnimationWithDuration(MAIN_LAYOUT_FADE_DURATION);
-		
-		fadeIn_main.setAnimationListener(new AnimationListener() {
-			
-			@Override
-			public void onAnimationStart(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				showLoginBox();
-			}
-		});
-		main_layout.setAnimation(fadeIn_main);
-	}
 	
 	private class LoginTask extends FetchJSONTaskPost<Account>{
 
@@ -168,7 +130,6 @@ public class SplashScreen extends FragmentActivity {
 				
 				@Override
 				public void run() {
-					showLoginBox();
 				}
 			});
 			
